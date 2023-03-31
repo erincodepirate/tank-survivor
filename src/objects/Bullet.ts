67d19cd30855e469012d17a08d176455961dceca
Erin Clark <erincodepirate@gmail.com>
2023-03-30 17:17:29 -0700

    Split bullet class into its own file

---
+++ src/objects/Bullet.ts
@@ -0,0 +1,35 @@
+import Phaser from 'phaser';
+
+export class Bullet {
+    bulletRect: Phaser.GameObjects.Rectangle;
+    distanceTraveled: number = 0;
+  
+    constructor(add: any, x: number, y: number) {
+      this.bulletRect = add.rectangle(x + 64, y + 64, 5, 15, 0xffbb00, 1);
+    }
+  
+    destroy() {
+      this.bulletRect.destroy();
+    }
+  
+    public get x(): number {
+      return this.bulletRect.x;
+    }
+  
+    public set x(newX: number) {
+      this.bulletRect.x = newX;
+    }
+  
+    public get y(): number {
+      return this.bulletRect.y;
+    }
+  
+    public set y(newY: number) {
+      this.bulletRect.y = newY;
+    }
+  
+    public get rotation(): number {
+      return this.bulletRect.rotation;
+    }
+  
+  }
--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,39 +1,6 @@
 import Phaser from 'phaser';
 import config from '../config';
-
-class Bullet {
-  bulletRect: Phaser.GameObjects.Rectangle;
-  distanceTraveled: number = 0;
-
-  constructor(add: any, x: number, y: number) {
-    this.bulletRect = add.rectangle(x + 64, y + 64, 5, 15, 0xffbb00, 1);
-  }
-
-  destroy() {
-    this.bulletRect.destroy();
-  }
-
-  public get x(): number {
-    return this.bulletRect.x;
-  }
-
-  public set x(newX: number) {
-    this.bulletRect.x = newX;
-  }
-
-  public get y(): number {
-    return this.bulletRect.y;
-  }
-
-  public set y(newY: number) {
-    this.bulletRect.y = newY;
-  }
-
-  public get rotation(): number {
-    return this.bulletRect.rotation;
-  }
-
-}
+import { Bullet } from '../objects/Bullet';
 
 export default class TankSurvivor extends Phaser.Scene {
 
